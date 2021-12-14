e16fa1396ec480b4d3506dd7fb08ac24f83ad700
jfarroyo291 <j-a-m291@hotmail.com>
2021-12-14 11:42:48 -0500

    ci(db.js): Set correct db.js

--- models/db.js
+++ models/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dotenv = require('dotenv').config();
 
 const msg = require('mongoose');
@@ -7,14 +6,4 @@ msg.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.e
     console.log("Success!! Database conected!")
 });
 
-=======
-const dotenv = require('dotenv').config();
-
-const msg = require('mongoose');
-msg.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, (err, db) => {
-    if (err) throw err;
-    console.log("Success!! Database conected!")
-});
-
->>>>>>> 73ec1c64de61a20f2c8896c1ffb4b60a0b19c786
 module.exports = msg;
